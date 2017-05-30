90ac7adc9d62ab86a11eb99b9cdf0e2a8ef1d312
John Strebeck <thegreatplanetmojang@gmail.com>
2017-05-29 23:13:18 -0500

    Removed source file form discluded files

---
+++ UniversalArchiver/DEBUG/DebugConsole.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace UniversalArchiver.DEBUG
+{
+    partial class DebugConsole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.rtbConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbConsole.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbConsole.ForeColor = System.Drawing.SystemColors.Info;
+            this.rtbConsole.Location = new System.Drawing.Point(0, 0);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.Size = new System.Drawing.Size(712, 432);
+            this.rtbConsole.TabIndex = 0;
+            this.rtbConsole.Text = "";
+            // 
+            // DebugConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 432);
+            this.Controls.Add(this.rtbConsole);
+            this.Name = "DebugConsole";
+            this.ShowIcon = false;
+            this.Text = "Debug Console";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbConsole;
+    }
+}
---
+++ UniversalArchiver/DEBUG/DebugConsole.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversalArchiver.DEBUG
+{
+    public partial class DebugConsole : Form
+    {
+        public DebugConsole()
+        {
+            this.InitializeComponent();
+
+            this.Load += this.OnLoad;
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            TextWriter writer = new TextBoxStreamWriter(this);
+            Console.SetOut(writer);
+
+            Program.ConsoleCreated = true;
+        }
+
+        private void AddTexttoConsole(char text)
+        {
+            this.Invoke(new Action(() =>
+            {
+                this.rtbConsole.AppendText(text.ToString());
+            }));
+        }
+
+
+        private class TextBoxStreamWriter : TextWriter
+        {
+            private readonly DebugConsole output = null;
+
+            public TextBoxStreamWriter(DebugConsole output)
+            {
+                this.output = output;
+            }
+
+            public override void Write(char value)
+            {
+                base.Write(value);
+                this.output.AddTexttoConsole(value);
+            }
+
+            public override Encoding Encoding => System.Text.Encoding.UTF8;
+        }
+    }
+}
